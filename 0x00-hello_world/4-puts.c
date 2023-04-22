3cf5f4506c569f2cde16ce329971ec1d6a68f81e
Neeytech <ogunleyeoyindamola22@gmail.com>
2023-04-22 06:38:58 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - main entry point
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
  * return : always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
 }
