c49193045be2daeb3f93edf4e724acbd42dc3435
Neeytech <ogunleyeoyindamola22@gmail.com>
2023-04-22 07:44:16 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  *
  * return : always 0 (success)
  */
-int main(void)
+int main(void
+		)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return(0);
 }
