53053dd031af26c837f771c22d9418f433baba37
Neeytech <ogunleyeoyindamola22@gmail.com>
2023-04-24 10:59:46 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,6 @@
 * description - Write a C program that prints exactly and that piece of
 * art is useful\"
 *
-- Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
-*
 * Return: always 0 (Success)
 */
 int main(void)
