b38134d17f8db86e98cd27b1edb35d62aca4bb35
Neeytech <ogunleyeoyindamola22@gmail.com>
2023-04-28 00:28:03 -0700

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,8 @@ void more_numbers(void)
 		int i, j;
 
 		for (i = 1; i <= 10; i++)
-		{
-			if (j = 0); 
-			(j <= 14; j++);
+		{	
+			for (j = 0; j <= 14; j++)
 			{
 				if (j >= 10)
 					_putchar('1');
