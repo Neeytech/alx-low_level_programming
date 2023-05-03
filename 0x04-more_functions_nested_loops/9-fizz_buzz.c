9f361156f3fec087a7a27a139f23f8adeed08cd5
Neeytech <ogunleyeoyindamola22@gmail.com>
2023-05-02 23:33:49 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,17 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
-	}
-		if (i == 1)
-	{
-			printf("%d", i);
 	} else
 	{
 			printf("%d", i);
 	}
 	printf("\n");
-	return (0);
+	{
+		fizz_buzz();
+	}
+		return (0);
 }
